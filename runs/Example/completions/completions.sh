f3a5bace1279eab52054c1d1ae623f0a4ede3012
akashdhruv <akashdhruv@gwmail.gwu.edu>
2024-01-30 13:46:02 -0600

    Add new examples

--- runs/Example/completions/completions.sh
+++ runs/Example/completions/completions.sh
@@ -1,6 +1,4 @@
-export CUDA_VISIBLE_DEVICES=""
-
-torchrun --nnodes 1 --nproc_per_node 1 $CODELLAMA_HOME/example_completion.py \
+torchrun --nproc_per_node 1 $CODELLAMA_HOME/example_completion.py \
 	--ckpt_dir $MODEL_HOME/codellama/CodeLlama-7b/jobnode.archive/2024-01-30 \
 	--tokenizer_path $MODEL_HOME/codellama/CodeLlama-7b/jobnode.archive/2024-01-30/tokenizer.model \
 	--max_seq_len 128 --max_batch_size 2
